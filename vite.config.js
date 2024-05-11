87c5d5d500b8b4b18765ffab041436b5e227a34e
jannahgiee <jjtravelmemories@gmail.com>
2024-05-11 23:07:55 +0800

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/portfolio/',
   plugins: [react()],
-  assetsInclude: ['**/*.glb'],
-})
+  build: {
+    rollupOptions: {
+      external: ['react-router', 'react-router-dom']
+    }
+  },
+  assetsInclude: ['**/*.glb']
+});
