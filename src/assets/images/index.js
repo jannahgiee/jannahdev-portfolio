86cf6c393211d5af5741967abc257c6d8fcdedc6
jannahgiee <jjtravelmemories@gmail.com>
2024-05-12 00:58:53 +0800

    Fix images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,21 +1,6 @@
 
-import circleBlue from './circleBlue.svg'
-import circleRed from './circleRed.svg'
-import circleYellow from './circleYellow.svg'
-import circleGreen from './circleGreen.svg'
-import readmeMockup from './readme-mockup.png'
-import appleAppMockup from './apple-app-mockup.webp'
-import artMockup from './art-mockup.webp'
 import  dohle from './doehle.png'
 
 export {
   dohle,
-  circleBlue,
-  circleRed,
-  circleYellow,
-  circleGreen,
-  readmeMockup,
-  appleAppMockup,
-  artMockup,
-
 }
