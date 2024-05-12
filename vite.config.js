c10bc042d2316463657f1c43fa16ddbc4fe8f224
jannahgiee <jjtravelmemories@gmail.com>
2024-05-12 21:56:59 +0800

    Updated

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,18 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/jannahdev-portfolio/',
   plugins: [react()],
+  server: {
+    fs: {
+      // Allow serving files outside of `public` directory
+      strict: false
+    },
+    // Fallback to index.html for all routes
+    proxy: {
+      '/': {
+        target: 'https://jannahgiee.github.io/jannahdev-portfolio/', // Replace with your server address
+        rewrite: (path) => path === '/' ? '/index.html' : '/index.html'
+      }
+    }
+  },
   assetsInclude: ['**/*.glb'],
 })
